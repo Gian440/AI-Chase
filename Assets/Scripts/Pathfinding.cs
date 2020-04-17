ae621bd658fd5ff52e5a7297f2b144e984bee829
Gian440 <bgiandiri@gmail.com>
2020-04-17 10:37:14 +1000

    17/04

---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+
+public class Pathfinding : MonoBehaviour
+{
+    #region Variables
+    public float Speed = 5.0f;
+    public GameObject[] Waypoint;
+    public GameObject AiSprite;
+    public float MinDistanceToWaypoint;
+    public GameObject playerObject;
+    public float ChasePlayerDistance;
+    public float DamagePlayerDistance;
+    private int CurrentWaypoint = 0;
+    public GameObject YouLose;
+    public PlayerController playerController;
+    float AttackCooldown = 1f;
+    public float DealDamage;
+    float TimeOfAttack = float.MinValue;
+    public float AiHealth = 3;
+    public GameObject aiBase;
+    public GameObject aiBaseTrigger;
+    #endregion
+
+
+
+    void Update()
+    #region Victory Lap
+    {
+        //increases the ai speed when the player is dead to simulate a victory lap
+        if (playerController.Health <= 0)
+        {
+            Speed = 50;
+            Patrol();
+
+        }
+        #endregion
+        #region AI Movement
+        //ai gors to base when health is low
+        else if (playerController.Health > 0 && AiHealth <= 1)
+        {
+            MoveAI(aiBase.transform.position, Speed);
+        }
+        //creates a cooldown between attacks
+        else if (Vector2.Distance(playerObject.transform.position, AiSprite.transform.position) < DamagePlayerDistance)
+        {
+            if (Time.time > TimeOfAttack + AttackCooldown)
+            {
+                playerController.TakeDamage(DealDamage);
+                TimeOfAttack = Time.time;
+            }
+        }
+        //makes the ai chase player in a certain range instead of following waypoints
+        else if (Vector2.Distance(playerObject.transform.position, AiSprite.transform.position) < ChasePlayerDistance)
+        {
+            MoveAI(playerObject.transform.position, Speed);
+        }
+        //if no player is present then the ai will patrol using waypoints
+        else
+        {
+            Patrol();
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D aiBaseTrigger)
+    {
+        AiHealth = 4;
+    }
+    #endregion
+    private void MoveAI(Vector2 targetPosition, float speed)
+    #region MoveAI
+    {
+        
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+    }
+    #endregion
+    private void Patrol()
+    #region Patrol
+    {
+
+        //resets the waypoint order back to 0 to allow an endless loop for the ai to follow
+        if (CurrentWaypoint >= Waypoint.Length)
+        {
+            CurrentWaypoint = 0;
+        }
+
+        MoveAI(Waypoint[CurrentWaypoint].transform.position, Speed);
+
+        if (Vector2.Distance(transform.position, Waypoint[CurrentWaypoint].transform.position) <= 0.01f)
+        {
+            CurrentWaypoint++;
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+    //different variables to alter properties of AI and player
+    public float Health = 0;
+
+    public float Speed;
+    public Pathfinding playerController;
+    private Rigidbody2D _PlayerRigidBody;
+
+    private float _MoveHorizontal;
+    private float _MoveVertical;
+
+    #endregion
+    void Start()
+    #region Rigidbody
+    {
+        //creates a rigidbody and allows the use of Unity's physics engine
+        _PlayerRigidBody = GetComponent<Rigidbody2D>();
+    }
+    #endregion
+   
+    public void TakeDamage(float damage)
+    #region Damage 
+    {
+        Health -= damage;
+    }
+    #endregion
+
+    private void Update()
+    {
+        //Makes the space bar take 1 point of health from the AI
+        if((Vector2.Distance(playerController.playerObject.transform.position, playerController.AiSprite.transform.position) < playerController.ChasePlayerDistance) && Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            playerController.AiHealth -= 1;
+
+
+        }
+    }
+    void FixedUpdate()
+     #region Movement
+    {
+        //allows the player to move if health is above 0, otherwise they freeze
+        if (Health > 0)
+        {
+            _MoveHorizontal = Input.GetAxis("Horizontal");
+            _MoveVertical = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2(_MoveHorizontal, _MoveVertical);
+
+            _PlayerRigidBody.AddForce(movement * Speed);
+        }
+        #endregion
+    }
+}
